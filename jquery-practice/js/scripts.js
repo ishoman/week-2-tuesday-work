003503f50edb1080faf55a01e3d98f6c212a19f5
Isaac Shoman and Jared Lutz <jared@>
2018-03-20 10:04:54 -0700

    Add favorite color background buttons that can alternate between three colors.

--- jquery-practice/js/scripts.js
+++ jquery-practice/js/scripts.js
@@ -8,4 +8,19 @@ $(document).ready(function(){
     $("#walrus-showing").fadeToggle();
     $("#walrus-hidden").fadeToggle();
   });
+
+  $("button#green").click(function(){
+    $("body").removeClass();
+    $("body").addClass("green-background")
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background")
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background")
+  });
 });
